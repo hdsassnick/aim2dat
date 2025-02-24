9c32054fbfa6d1ed0d0245c1dcbdaa4018987199
holger <holger.sassnick@gmx.net>
2025-02-24 14:49:06 +0100

    Update.

--- aim2dat/mc/engines.py
+++ aim2dat/mc/engines.py
@@ -1,17 +1,63 @@
 
 
-# TODO move this to ext_interfaces.
-#from mpi4py import MPI
+
 import numpy as np
 
 
+from aim2dat.mc.moves import InsertComponent, RemoveComponent
 from aim2dat.ext_interfaces import _return_ext_interface_modules
 from aim2dat.strct.ext_manipulation import add_structure_random
 
 
-class MonteCarloSimulation:
+# TODO implement general logging function.
+class _BaseMonteCarlo:
 
-    def __init__(self, structure, components, n_components, moves, temperature: float, n_steps: int, random_seed: int = None, ase_calculator = None):
+    def _prepare_structure(self):
+        if len(self.components) > 1:
+            raise ValueError("Simulations with multiple components are not yet supported.")
+
+        comp = self.components[0]
+        n_comp = self.n_components[0]
+        if comp.label is None: # TODO set label internally.
+            raise ValueError(f"Component label neeeds to be set.")
+
+        # TODO expose comp_labels to be used in moves or store in structure??
+        comp_labels = []
+        if self.structure.kinds is None:
+            self.structure.kinds = [None] * len(structure)
+        else:
+            for idx, kind in enumerate(self.structure.kinds):
+                if kind is None:
+                    continue
+                if comp.label in kind and kind not in comp_labels:
+                    comp_labels.append(kind)
+
+        print(f"Adding {n_comp - len(comp_labels)} molecules of component {comp.label}.")
+        for i in range(len(comp_labels), n_comp):
+            comp.kinds = [f"{comp.label}_{i}"] * len(comp)
+            self.structure = add_structure_random(self.structure, guest_structure=comp, dist_threshold=1.2, change_label=False)
+
+    def _get_energy(self, structure):
+        # TODO outsource into external function..
+        if "ref_energy" not in structure.attributes:
+            if self.ase_calculator is not None:
+                backend_module = _return_ext_interface_modules("ase_calculator")
+                # TODO check if ref_energy is a good key, maybe mc_energy better?
+                structure.set_attribute("ref_energy", backend_module.get_potential_energy(structure, self.ase_calculator))
+            else:
+                raise ValueError("No viable backend found to evaluate the energy.") # TODO make error message more informative
+        return structure.attributes["ref_energy"]
+
+    def _print_stdout(self, step, energy):
+        if step % 1000 == 0:
+            print(f"Step: {step}, energy: {energy}")
+
+
+
+
+class MonteCarlo(_BaseMonteCarlo):
+
+    def __init__(self, structure, components, n_components, moves, temperature: float, n_steps: int, n_store: int = None, random_seed: int = None, ase_calculator = None):
         # TODO Distinguish between canonical and grand canonical MC
         # TODO Add properties to validate input parameters.
         self.structure = structure
@@ -19,69 +65,88 @@ class MonteCarloSimulation:
         self.n_components = n_components
         # TODO implement custom probability weights?
         self.moves = moves
+        self.structures = []
 
         self.temperature = temperature
-        self.pressures = None # TODO implement this?
         self.n_steps = n_steps # TODO implement logic with steps and cycles.
-        self.eos = None
+        self.n_store = 10 if n_store is None else n_store
 
         self.random_seed = random_seed
         self.ase_calculator = ase_calculator
 
-
-        self._prepare_structures()
+        self._prepare_structure()
 
     def run(self):
         # TODO outsource into external function..
-        if self.ase_calculator is not None:
-            backend_module = _return_ext_interface_modules("ase_calculator")
-            self.structure.set_attribute("ref_energy", backend_module.get_potential_energy(self.structure, self.ase_calculator))
-        else:
-            raise ValueError("No viable backend found to evaluate the energy.") # TODO make error message more informative
+        self._get_energy(self.structure)
 
         rng = np.random.default_rng(seed=self.random_seed)
-
         for i in range(self.n_steps):
-            print("step:", i)
             move_cls = self.moves[int(rng.random() * len(self.moves))] # TODO change this to implement
-            print(move_cls)
             rand_nrs = [rng.random() for _ in range(move_cls.n_rand_nrs + 1)]
             move = move_cls(
                 structure=self.structure,
                 component=self.components[0],
                 ase_calculator=self.ase_calculator,
             )
-            valid_move = move.perform_move(rand_nrs[:-1])
-            if not valid_move:
-                continue
-
+            move.perform_move(rand_nrs[:-1])
             if move.accept_move(rand_nrs[-1], self.temperature): # TODO fix optional parameters.
                 self.structure = move.new_structure
 
-    def _prepare_structures(self):
-        if len(self.components) > 1:
-            raise ValueError("Multipe compontents simulations are not yet supported")
+            # TODO move this to a logger function.
+            if i % int(self.n_steps / self.n_store) == 0:
+                self.structures.append(self.structure)
+            self._print_stdout(i, self.structure.attributes["ref_energy"])
 
-        if self.components[0].label is None:
-            raise ValueError(f"Component label neeeds to be set.")
 
-        # Add n component:
-        component = self.components[0]
-        component_label = self.components[0].label
-        n_component = self.n_components[0]
-        comp_labels = []
-        if self.structure.kinds is None:
-            self.structure.kinds = [None] * len(self.structure)
-        else:
-            for idx, kind in enumerate(self.structure.kinds):
-                if kind is None:
-                    continue
-                if component_label in kind and kind not in comp_labels:
-                    comp_labels.append(kind)
 
-        for i in range(len(comp_labels), n_component):
-            component.kinds = [f"{component_label}_{i}"] * len(component)
-            self.structure = add_structure_random(self.structure, guest_structure=component, dist_threshold=1.2, change_label=False)
+class TransitionMatrixMonteCarlo(_BaseMonteCarlo):
+    def __init__(self, structure, components, n_components, n_steps: int, random_seed: int = None, ase_calculator = None):
+        # TODO Distinguish between canonical and grand canonical MC
+        # TODO Add properties to validate input parameters.
+        self.structure = structure
+        self.components = components
+        self.n_components = n_components
+        # TODO implement custom probability weights?
+
+        self.n_steps = n_steps # TODO implement logic with steps and cycles.
+        self.energy_penalty = 1.0e5
+        self.insert_energies = []
+        self.remove_energies = []
+
+
+        self.random_seed = random_seed
+        self.ase_calculator = ase_calculator
+
+        self._prepare_structure()
+
+    def run(self):
+        rng = np.random.default_rng(seed=self.random_seed)
+        # TODO calculate energy if not given.
+        e_structure = self._get_energy(self.structure)
+        e_component = self._get_energy(self.components[0])
+        for i in range(self.n_steps):
+            self.insert_energies.append(self._move_wrapper(InsertComponent, rng) - e_structure - e_component)
+            e_rm = 0.0
+            if self.n_components[0] > 0:
+                self.remove_energies.append(self._move_wrapper(RemoveComponent, rng) - e_structure + e_component)
+                e_rm = self.remove_energies[-1]
+            self._print_stdout(i, f"{e_rm}/{self.insert_energies[-1]}")
+
+    def _move_wrapper(self, move_cls, rng):
+        rand_nrs = [rng.random() for _ in range(move_cls.n_rand_nrs)]
+        move = move_cls(
+            structure=self.structure,
+            component=self.components[0],
+            ase_calculator=self.ase_calculator,
+        )
+        move.perform_move(rand_nrs)
+        e = move.get_energy()
+        if e is None:
+            return self.energy_penalty
+        return e
+
+
 
 
 
--- aim2dat/mc/moves.py
+++ aim2dat/mc/moves.py
@@ -25,77 +25,112 @@ class BaseMove(abc.ABC):
 
     def accept_move(self, rand_nr, temperature):
         # TODO add presssure/fugacity
-        self.get_energy(self.new_structure)
-        e_diff = self.new_structure.get_attribute("ref_energy") - self.structure.get_attribute("ref_energy")
+        e = self.get_energy()
+        if e is None:
+            return False
+
+        e_diff = e - self.structure.get_attribute("ref_energy")
         # TODO check here if energies are available?
-        print(e_diff, self.new_structure.get_attribute("ref_energy"), self.structure.get_attribute("ref_energy"))
-        print(rand_nr < min(1.0, np.exp(-1.0 * e_diff / (temperature * constants.kb * energy.j))))
         return rand_nr < min(1.0, np.exp(-1.0 * e_diff / (temperature * constants.kb * energy.j)))
 
-    def get_energy(self, structure):
+    def get_energy(self):
+        if self.new_structure is None:
+            return None
+
         # TODO implement lammps and openmm backends and make backend module
         if self.ase_calculator is not None:
             backend_module = _return_ext_interface_modules("ase_calculator")
-            structure.set_attribute("ref_energy", backend_module.get_potential_energy(structure, self.ase_calculator))
+            energy = backend_module.get_potential_energy(self.new_structure, self.ase_calculator)
+            self.new_structure.set_attribute("ref_energy", energy)
+            return energy
         else:
             raise ValueError("No viable backend found to evaluate the energy.")
 
-    def get_random_component_indices(self, rand_nr):
+    def get_all_component_indices(self):
         indices = {}
         for idx, kind in enumerate(self.structure.kinds):
             if kind is None:
                 continue
             if self.component.label in kind:
                 indices.setdefault(kind, []).append(idx)
+        return indices
+
+
+    def get_random_component_indices(self, rand_nr):
+        indices = self.get_all_component_indices()
         kind = list(indices.keys())[int(rand_nr * len(indices))]
         return kind, indices[kind]
+
+
+def _insert_component(structure, component, component_label, rand_nrs):
+    # TODO use add_structure_random and adjust interface to do so.
+    component = rotate_structure(component, angles=rand_nrs[0] * 360.0, vector=rand_nrs[1:4], change_label=False)
+    component.kinds = [component_label] * len(component)
+    pos = (np.array(structure.cell).T).dot(np.array(rand_nrs[4:7]))
+    try:
+        new_structure = add_structure_position(
+            structure, guest_structure=component, position=pos, dist_threshold=1.25, change_label=False
+        )
+    except ValueError: # TODO be more specific on error message.
+        return None
+    return new_structure
 
 
 class RotateComponent(BaseMove):
     n_rand_nrs = 5
 
     def perform_move(self, rand_nrs):
-        comp_label, comp_indices = self.get_random_component_indices(rand_nrs[0])
+        _, site_indices = self.get_random_component_indices(rand_nrs[0])
         try:
             self.new_structure = rotate_structure(
                 self.structure, angles=rand_nrs[1] * 360.0, vector=rand_nrs[2:5],
-                site_indices=comp_indices, change_label=False, dist_threshold=1.25
+                site_indices=site_indices, change_label=False, dist_threshold=1.25
             )
-        except ValueError:
-            return False
-        return True
+        except ValueError: # TODO be more specific on error message.
+            pass
 
 
 class TranslateComponent(BaseMove):
     n_rand_nrs = 5
 
     def perform_move(self, rand_nrs):
-        comp_label, comp_indices = self.get_random_component_indices(rand_nrs[0])
+        _, site_indices = self.get_random_component_indices(rand_nrs[0])
+        #comp_indices = self.get_component_indices()
+        #kind = list(comp_indices.keys())[int(rand_nrs[0] * len(comp_indices))]
         v = np.array(rand_nrs[1:4]) - 0.5
         v *= rand_nrs[4] * np.linalg.norm(v)
         try:
-            self.new_structure = translate_structure(self.structure, site_indices=comp_indices, vector=v, dist_threshold=1.25, change_label=False)
-        except ValueError:
-            return False
-        return True
+            self.new_structure = translate_structure(self.structure, site_indices=site_indices, vector=v, dist_threshold=1.25, change_label=False)
+        except ValueError: #TODO be more specific on error message or add new error for AtomsTooClose.
+            pass
+
+
+class RemoveComponent(BaseMove):
+    n_rand_nrs = 1
+
+    def perform_move(self, rand_nrs):
+        _, site_indices = self.get_random_component_indices(rand_nrs[0])
+        self.new_structure = self.structure.delete_atoms(site_indices=site_indices, change_label=False)
+
+
+class InsertComponent(BaseMove):
+    n_rand_nrs = 7
+
+    def perform_move(self, rand_nrs):
+        comp_indices = self.get_all_component_indices()
+        ind_numbers = [int(v.split("_")[-1]) for v in comp_indices]
+        max_idx = max(ind_numbers) if len(ind_numbers) > 0 else 0
+        new_kind = f"{self.component.label}_{max_idx + 1}"
+        self.new_structure = _insert_component(self.structure, self.component, new_kind, rand_nrs)
 
 
 class ReinsertComponent(BaseMove):
     n_rand_nrs = 8
 
     def perform_move(self, rand_nrs):
-        comp_label, comp_indices = self.get_random_component_indices(rand_nrs[0])
-        new_strct = self.structure.delete_atoms(site_indices=comp_indices, change_label=False)
-        component = rotate_structure(self.component, angles=rand_nrs[1] * 360.0, vector=rand_nrs[2:5], change_label=False)
-        component.kinds = [comp_label] * len(component)
-        pos = (np.array(self.structure.cell).T).dot(np.array(rand_nrs[5:8]))
-        try:
-            self.new_structure = add_structure_position(
-                new_strct, guest_structure=component, position=pos, dist_threshold=1.25, change_label=False
-            )
-        except ValueError:
-            return False
-        return True
+        label, site_indices = self.get_random_component_indices(rand_nrs[0])
+        new_strct = self.structure.delete_atoms(site_indices=site_indices, change_label=False)
+        self.new_structure = _insert_component(new_strct, self.component, label, rand_nrs[1:])
 
 
 
